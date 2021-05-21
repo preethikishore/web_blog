e619f820ca434980ad7d192978c4a0e99a18aef9
preethikishore <preethikishre@gmail.com>
2021-05-21 23:20:02 +0530

    newupdate

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
 const express = require("express");
 const bodyPareser = require("body-parser");
 const ejs = require("ejs");
-var _ = require('lodash');
+// var _ = require('lodash');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/blogDB", 
+{
+ useNewUrlParser: true,
+ useUnifiedTopology: true
+}
+);
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
+
+const postSchema = {
+    
+    title: String,
+    content : String
+}
+const Post = mongoose.model("Post",postSchema);
 const app = express();
 app.use(express.static("public"));
 app.use(bodyPareser.urlencoded({extended : true}));
 app.set('view engine' , 'ejs');
 app.get("/",function(req,res)
 {
-    res.render('home',{
-        dataOne : homeStartingContent,
-        posts : posts
+    Post.find({},function(err,posts)
+    {
+        
     
+          res.render('home',  {
+            dataOne : homeStartingContent,
+            posts : posts 
+        });
     });
    
 
 })
 app.get("/home",function(req,res)
 {
-    res.render('home', {dataOne : homeStartingContent,
-                         posts : posts });
+
+    Post.find({},function(err,posts)
+    {
+        res.render('home', {dataOne : homeStartingContent,
+            posts : posts });
+    })
+    
 
 
 })
@@ -48,34 +71,53 @@ app.get("/compose",function(req,res)
 })
 app.post("/compose",function(req,res)
 {
+    const post = new Post({
+        title  : req.body.postTitle,
+        content : req.body.postbody
+
+    })
     
-    const post = {
-        post_title : req.body.postTitle ,
-        post_body : req.body.postbody
-    };
-  posts.push(post);
-  res.redirect('/');
+  post.save(function(err)
+  {
+      if(!err)
+      {
+        res.redirect('/');
+      }
+  });
+ 
 })
 
-app.get("/post/:testing",function(req,res)
-{
-    
- const requestedTitle =  _.lowerCase(req.params.testing);
- posts.forEach(function(post)
- {
-     const postTitle = _.lowerCase(post.post_title);
-     
-     if(postTitle === requestedTitle)
-     {
-        
-        res.render('post',{
-            postTitle : post.post_title,
-            postContent : post.post_body
 
-         })
-     }
- })
-})
+app.get('/post/:postId', (req, res) => {
+    var ObjectID=require('mongodb').ObjectID;
+    console.log('id: ' + req.params.postId + ' type: ' + typeof req.params.postId);
+    Post.findOne({_id : ObjectID(req.params.postId) }).then((post) => {
+      if(!post) { //if page not exist in db
+        return res.status(404).send('Page not found');
+      }
+      res.render('post', { //page  exist
+        postTitle: post.title,
+        postContent: post.content
+      });
+    }).catch((e) => {//bad request 
+      res.status(400).send(e);
+    });
+  });
+
+  app.post('/delete',function(req,res)
+  {
+    
+      Post.findByIdAndRemove(req.body.button ,function(err)
+      {
+         if(err)
+         {
+             console.log(err)
+         }else{
+             console.log("Deleted Successfully");
+             res.redirect("/");
+         }
+      })
+  })
 
 app.listen(3000,function(req,res)
 {
